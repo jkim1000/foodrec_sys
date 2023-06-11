55535a23aabf12a51a03587409f02e78cfdb70b6
Thomas <thomaskim1995@gmail.com>
2023-06-11 15:31:00 -0700

    setting up repo

---
+++ src/data/url_scrape.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# Jamie Oliver recipe website
+url = "https://www.jamieoliver.com/recipes/category/course/mains/"
+page = requests.get(url)
+
+# Initializing DataFrame to store the scraped URLs
+recipe_url_df = pd.DataFrame() 
+
+# BeautifulSoup enables to find the elements/tags in a webpage
+soup = BeautifulSoup(page.text, "html.parser")
+
+food_cat = ["mains", "snacks", "breakfast", "desserts"]
+
+# Selecting all the 'a' tags (URLs) present in the webpage and extracting 
+# their 'href' attribute
+recipe_urls = pd.Series([a.get("href") for a in soup.find_all("a")])
+
+# All the recipes contain '-' and the '/recipes/' etc etc ...
+recipe_urls = recipe_urls[(recipe_urls.str.count("-")>0) 
+                         & (recipe_urls.str.contains("/recipes/")==True)
+                         & (recipe_urls.str.contains("-recipes/")==True)
+                         & (recipe_urls.str.contains("course")==False)
+                         & (recipe_urls.str.contains("books")==False)
+                         & (recipe_urls.str.endswith("recipes/")==False)
+                         ].unique()
+
+# DataFrame to store the scraped URLs
+df = pd.DataFrame({"recipe_urls":recipe_urls})
+df['recipe_urls'] = "https://www.jamieoliver.com" + df['recipe_urls'].astype('str')
+
+df.to_csv(r"/Users/tk/Desktop/foodrec_sys/data/recipe_urls.csv", sep="\t", index=False)
+
